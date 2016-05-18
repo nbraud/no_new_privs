c62f36a860e609d9f4ab3efbb403949c8e11f431
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2016-05-18 14:52:29 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/prctl.h>
+
+int main(int argc, char *const * argv) {
+  if (argc <= 1) {
+    fprintf(stdout, "Too few arguments");
+    return EXIT_FAILURE;
+  }
+
+  if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) != 0)
+    perror("Failed to set NO_NEW_PRIVS");
+
+  execvp(argv[1], argv+1);
+  err(EXIT_FAILURE, "Failed to start %s", argv[1]);
+}
